e87d7d62631aa0e74f1120637bbdbf7a7bbd9738
Emmanuel Gonzalez <emmanuel.gonzalez@mavs.uta.edu>
2017-10-10 09:37:58 -0500

    adding files to create the main window

---
+++ main_window.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#include "publication.h"
+
+#include <gtkmm.h>
+
+class Main_window : public Gtk::Window {
+  public:
+    Main_window();
+    virtual ~Main_window();
+  protected:
+
+};
+#endif
